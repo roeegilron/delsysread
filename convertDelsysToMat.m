b3fb87177fc390d995d6b8da9acdcedd00bca2ff
Roee Gilron <roee@RoeeG.ucsf.edu>
2018-09-04 16:40:29 -0700

    fixed bug with convert

--- convertDelsysToMat.m
+++ convertDelsysToMat.m
@@ -1,4 +1,4 @@
-function fileout = convertDelsysToMat(varargin)
+function dataraw = convertDelsysToMat(varargin)
 if nargin==1
     [pn,fn,ext] = fileparts(varargin{1});
     dirout = pn; 
@@ -11,7 +11,14 @@ if exist(fullfile(dirout,[fn '.mat']),'file')
 else
     fnmread = fullfile(dirout,fn);
     dataraw = readtable(fnmread);
-    save(fullfile(dirout,[fn '.mat']),'dataraw');
+    % chekc if data is larger than 2GB and needs to be saved using 7.3 flag
+    varinfo = whos('dataraw');
+    saveopt = '';
+    if varinfo.bytes >= 2^31
+        saveopt = '-v7.3';
+    end
+    save(fullfile(dirout,[fn '.mat']),'dataraw',saveopt);
 end
 fileout = fullfile(dirout,[fn '.mat']);
+load(fileout,'dataraw'); 
 end
